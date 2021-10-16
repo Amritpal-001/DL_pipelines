72311e2f45c4476ce57e0fc4cc3d1b90ce8b5593
Amritpal singh <50632243+Amritpal-001@users.noreply.github.com>
2021-10-16 15:46:25 +0530

    Update meter.py

--- amrit/metrics/meter.py
+++ amrit/metrics/meter.py
@@ -3,55 +3,6 @@ from .classification_metrics import *
 from .regression_metrics import *
 import numpy as np
 
-
-from sklearn.metrics import accuracy_score , auc , balanced_accuracy_score , classification_report , confusion_matrix, \
-    f1_score , jaccard_score, matthews_corrcoef, multilabel_confusion_matrix, precision_score , \
-    recall_score ,roc_curve , top_k_accuracy_score, cohen_kappa_score
-
-#https://scikit-learn.org/stable/modules/model_evaluation.html#classification-metrics
-
-def get_accuracy_score(preds, gt):
-    return( accuracy_score(preds, gt))
-
-def get_auc(preds, gt):
-    return( auc(preds, gt))
-
-def balanced_accuracy_score(preds, gt):
-    return( balanced_accuracy_score(preds, gt))
-
-def get_classification_report(preds, gt):
-    return( classification_report(preds, gt))
-
-def get_confusion_matrix(preds, gt):
-    return( confusion_matrix(preds, gt))
-
-def get_f1_score(preds, gt):
-    return( f1_score(preds, gt))
-
-def get_jaccard_score(preds, gt):
-    return( jaccard_score(preds, gt))
-
-def get_matthews_corrcoef(preds, gt):
-    return( matthews_corrcoef(preds, gt))
-
-def get_multilabel_confusion_matrix(preds, gt):
-    return( multilabel_confusion_matrix(preds, gt))
-
-def get_precision_score(preds, gt):
-    return( precision_score(preds, gt))
-
-def get_recall_score(preds, gt):
-    return( recall_score(preds, gt))
-
-def get_roc_curve(preds, gt):
-    return( roc_curve(preds, gt))
-
-def get_top_k_accuracy_score(preds, gt):
-    return( top_k_accuracy_score(preds, gt))
-
-def get_cohen_kappa_score(preds, gt):
-    return( cohen_kappa_score(preds, gt))
-
 def get_classification_statistics(preds, gt):
 
     auc = 0 #get_auc(preds, gt)
@@ -64,7 +15,6 @@ def get_classification_statistics(preds, gt):
     precision = get_precision_score(preds, gt)
     recall = get_recall_score(preds, gt)
     roc = get_roc_curve(preds, gt)
-    top_k_accuracy = get_top_k_accuracy_score(preds, gt)
     cohen = get_cohen_kappa_score(preds, gt)
 
     output = {
@@ -77,7 +27,6 @@ def get_classification_statistics(preds, gt):
         'precision' : precision,
         'recall' : recall,
         'roc' : roc,
-        'top_k_accuracy' : top_k_accuracy,
         'cohen' : cohen
     }
 
